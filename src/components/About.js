31237d4e3de4d28852229c948874e30a69650a1e
nidhikhunt15 <nidhikhunt83@gmail.com>
2023-01-09 17:56:18 +0530

    2

--- src/components/About.js
+++ src/components/About.js
@@ -14,12 +14,9 @@ export default function About(props) {
 
     }
 
-
-   
-
     return (
-        <div className='container' style={myStyle}>
-            <h1 className='my-3'>About us</h1>
+        <div className='container' >
+            <h1 className='my-3' style={{color: props.mode === 'dark'?'white':'#042743'}}>About us</h1>
             <div className="accordion" id="accordionExample" style={myStyle}>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
